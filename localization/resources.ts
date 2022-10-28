f5f8f1f17e21b90cb87f7b73832cd8fbe62b5ddc
Alexandre Moreira <brainmunchers@gmail.com>
2022-10-28 10:32:02 +0200

    add hooks and i18n

---
+++ localization/resources.ts
@@ -0,0 +1,38 @@
+import workEn from './locales/en-US/work.json';
+import workPt from './locales/pt-PT/work.json';
+
+import aboutEn from './locales/en-US/about.json';
+import aboutPt from './locales/pt-PT/about.json';
+
+import landingEn from './locales/en-US/landing.json';
+import landingPt from './locales/pt-PT/landing.json';
+
+import contactEn from './locales/en-US/contact.json';
+import contactPt from './locales/pt-PT/contact.json';
+
+import navbarEn from './locales/en-US/navbar.json';
+import navbarPt from './locales/pt-PT/navbar.json';
+
+import footerEn from './locales/en-US/footer.json';
+import footerPt from './locales/pt-PT/footer.json';
+
+const resources = {
+  'en-US': {
+    landing: landingEn,
+    work: workEn,
+    about: aboutEn,
+    contact: contactEn,
+    footer: footerEn,
+    navbar: navbarEn,
+  },
+  'pt-PT': {
+    landing: landingPt,
+    work: workPt,
+    about: aboutPt,
+    contact: contactPt,
+    footer: footerPt,
+    navbar: navbarPt,
+  },
+};
+
+export default resources;
